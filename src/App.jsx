1c6f2fcf295b2c7074fbbce5f8ad9cfdab69288a
Gabriel Ivasuc <gabrielivasuc@yahoo.com>
2024-12-11 14:46:58 +0200

    added useEffect hook in the app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useRef, useState } from 'react';
-
+import { useRef, useState, useEffect } from 'react';
+import { sortPlacesByDistance } from './loc.js';
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
 import Modal from './components/Modal.jsx';
@@ -10,6 +10,21 @@ function App() {
   const modal = useRef();
   const selectedPlace = useRef();
   const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const SortedPlaces = sortPlacesByDistance(
+        AVAILABLE_PLACES,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+
+      setAvailablePlaces(SortedPlaces);
+    });
+  }, []);
+
+
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -27,7 +42,14 @@ function App() {
       }
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
+
+
     });
+    const storedIds = JSON.parse(localStorage.getItem('selectePlaces')) || [];
+    if(storedIds.indexOf(id) === -1){
+      localStorage.setItem('selectedPlaces', JSON.stringify([id, ...storedIds]));
+    }
+
   }
 
   function handleRemovePlace() {
@@ -63,7 +85,8 @@ function App() {
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          places={availablePlaces}
+          fallbackText='Sorting places by distance...'
           onSelectPlace={handleSelectPlace}
         />
       </main>
